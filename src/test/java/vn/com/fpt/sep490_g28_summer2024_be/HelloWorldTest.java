44b465228d7c720e9937fe831868faadde5acefd
Nguyen Nam Hai <nguyennamhai.workvn@gmail.com>
2025-04-10 14:55:08 +0700

    Initial commit

---
+++ src/test/java/vn/com/fpt/sep490_g28_summer2024_be/HelloWorldTest.java
@@ -0,0 +1,11 @@
+package vn.com.fpt.sep490_g28_summer2024_be;
+
+import org.junit.jupiter.api.Test;
+
+public class HelloWorldTest {
+
+    @Test
+    void testHelloWorld() {
+        System.out.print("Hello World!\n");
+    }
+}
